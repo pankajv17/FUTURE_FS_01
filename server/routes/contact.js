24a50165680b3dee8059d9395f58fd5276fe5c52
Pankaj Verma <pankajkumarverma598@gmail.com>
2025-08-23 20:30:16 +0530

    Update contact.js
    add fitvise

--- server/routes/contact.js
+++ server/routes/contact.js
@@ -2,11 +2,13 @@ const express = require('express');
 const router = express.Router();
 const Message = require('../models/Message');
 
+app.use(express.static(path.join(__dirname, 'dist')));
 router.post('/', async (req, res) => {
   try {
     const { name, email, message } = req.body;
     const newMessage = new Message({ name, email, message });
     await newMessage.save();
+     res.sendFile(path.resolve(__dirname, 'dist/index.html'));
     res.status(200).json({ message: 'Message sent successfully' });
   } catch (err) {
     res.status(500).json({ error: 'Error sending message' });
@@ -14,3 +16,4 @@ router.post('/', async (req, res) => {
 });
 
 module.exports = router;
+
