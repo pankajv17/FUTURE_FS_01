06d3a1569e33e21bb718ad71a44de5b4cf0b124f
Pankaj Verma <pankajkumarverma598@gmail.com>
2025-08-23 22:31:11 +0530

    Update vite.config.js
    update backend server

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api":" https://future-fs-01-server-686q.onrender.com", // Backend server
+      "/api":"http://localhost:5000", // Backend server
     },
   },
 });
